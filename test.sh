55f91b8ab408374ae6f96fac74f63330ec6551ee
diohabara <diohabara@gmail.com>
2021-02-12 21:03:23 +0900

    fix: give permission

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global user.name "beet-aizu"
-git config --global user.email "aki.bdash@gmail.com"
+git config --global user.name "tectonic"
+git config --global user.email "tectonic@githubactionsample.com"
 
 git remote set-url origin https://diohabara:${GITHUB_TOKEN}@github.com/diohabara/sample-github-actions-compile-diff.git
 
